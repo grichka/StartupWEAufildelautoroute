6e0c7333abd0a19c2b3b89cf54129e3821d866fe
Christian Brel <ch.brel@gmail.com>
2013-03-16 22:41:35 +0100

    Added interaction on theme in colum right.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -50,10 +50,28 @@ require(['app', 'jquery', 'leaflet', 'bootstrap'], function (app, $) {
     L.marker([43.474847,6.514009], {icon: canard}).addTo(map)
     .bindPopup('Château Sainte-Roseline')
     ;//.openPopup();
+    
+    // add a marker for La Fourchette Gourmande, Le Cannet Des Maures - Restaurant
+    L.marker([43.390875,6.342639], {icon: canard}).addTo(map)
+    .bindPopup('La Fourchette Gourmande, Le Cannet Des Maures')
+    ;//.openPopup();
 
     $('#wonderfullGoButton').click(function(e) {
         e.preventDefault();
 
         map.setView([43.480826,6.278687], 6);
+    });
+    
+    
+    $('.theme').click(function(e){
+        if($(this).hasClass('theme_selected')) {
+            $(this).removeClass('theme_selected');
+            $(this).find('.btn').removeClass('btn-primary');
+            $(this).find('.btn').addClass('disabled');
+        } else {
+            $(this).addClass('theme_selected');
+            $(this).find('.btn').addClass('btn-primary');
+            $(this).find('.btn').removeClass('disabled');
+        }
     });
 });
